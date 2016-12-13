f7afc6469808bbf27eed3279702d9cc1fea007e3
Jane Philipps <jane.philipps@gmail.com>
2016-12-12 16:01:51 -0800

    parse csv file for API

--- app/views.py
+++ app/views.py
@@ -1,15 +1,21 @@
+import csv
 import json
 
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
-
 def index(request):
     return render_to_response('index.html')
 
+# Prepare API response
+response = []
+
+# TODO: update to relative path/find where to put csv file
+with open('/Users/janephilipps/code/moma-data/app/artist-gender-data.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        response.append(row)
+
 def api(request):
-    return HttpResponse(json.dumps([
-        { 'label': 'Male', 'count': 1000 },
-        { 'label': 'Female', 'count': 100 }
-    ]), content_type='application.json')
+    return HttpResponse(json.dumps(response), content_type='application.json')
