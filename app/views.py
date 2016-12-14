312382c083277ecdda1566a6f60fb4c0eadb7627
Jane Philipps <jane.philipps@gmail.com>
2016-12-13 22:12:45 -0800

    get data from DB

--- app/views.py
+++ app/views.py
@@ -10,47 +10,22 @@ from models import Artist
 def index(request):
     return render_to_response('index.html')
 
-# Prepare API response
 artists = Artist.objects.all()
-
-# JSON
-# { gender: [
-#     {'Male': 100 },
-#     {'Female': 10}
-#   ]
-# }
-
 
 def get_genders(artists):
 
-    data = dict()
-    gender = []
     male = 0
     female = 0
 
-    for a in Artist.objects.all():
+    for a in artists:
         if a.gender == 'M':
             male += 1
         elif a.gender == 'F':
             female += 1
 
-    print('M', male)
-    print('F', female)
-
-    gender.setdefault('male',list()).append(male)
-    gender.setdefault('female',list()).append(female)
-
-    data.setdefault('gender',dict()).append(gender)
-
-    return data
+    return {'gender': {'male': male, 'female': female}}
 
 response = get_genders(artists)
-
-# TODO: update to relative path/find where to put csv file
-# with open('/Users/janephilipps/code/moma-data/app/artist-gender-data.csv') as csvfile:
-#     reader = csv.DictReader(csvfile)
-#     for row in reader:
-#         response.append(row)
 
 def api(request):
     return HttpResponse(json.dumps(response), content_type='application.json')
